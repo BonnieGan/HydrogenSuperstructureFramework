a3d0706bb149bb7b83fe2b95ae0c722ad75db885
BonnieGan <85251654+BonnieGan@users.noreply.github.com>
2024-01-05 20:00:29 +0800

    Update utils_config.py

--- utils_config.py
+++ utils_config.py
@@ -1,30 +1,40 @@
 import os
+
+'''edit before running the code'''
 #project name
-project_name = 'case study'
+project_name = 'demo1'
 #database name
 db_name = 'ecoinvent 3.5 cutoff'
 #LCA result file name
-result_name = 'LCA results'
+result_name = 'LCA results_demo'
 #brightway2 compatible inventory table file name
-input_file_name = "input"
-#calculated LCIA result file name
-env_result = 'scenario results'
-
-#brightway2 compatible inventory table file path
-input_file_path = r"C:\Users\chenl\OneDrive - UBC\Desktop\bw2\hydrogen superstructure\files\CaseStudy\input_43.xlsx"
-#calculated LCIA result file path
-LCA_result_path = r"C:\Users\chenl\OneDrive - UBC\Desktop\bw2\hydrogen superstructure\files\CaseStudy\LCA results_7.xlsx"
-
+input_file_name = 'input_demo'
+#calculated optimization result file name
+env_result = 'solutions_demo'
 #ecoinvent database file path
-ei35_path = r"C:\Users\chenl\OneDrive - UBC\Desktop\ecoinvent 3.5_cutoff_ecoSpold02\ecoinvent 3.5_cutoff_ecoSpold02\datasets"
-#raw inventory table file path
-raw_data_path = r"C:\Users\chenl\OneDrive - UBC\Desktop\bw2\hydrogen superstructure\files\CaseStudy\inventory_CaseStudy.xlsx"
+ei35_path = '/Users/Bonnie/Desktop/ecoinvent 3.5_cutoff_ecoSpold02/datasets'
 #ecoinvent locations file path
-ei_35_loc_path = r"C:\Users\chenl\OneDrive - UBC\Desktop\bw2\hydrogen superstructure\files\ei35 locations.csv"
-#all exported file folder
-output_path = r"C:\Users\chenl\OneDrive - UBC\Desktop\bw2\hydrogen superstructure\files\CaseStudy"
+ei_35_loc_path = '/Users/Bonnie/Desktop/HydrogenSuperstructureFramework/ei35 locations.csv'
+#raw inventory table file path
+raw_data_path = '/Users/Bonnie/Desktop/demo/inventory_demo.xlsx'
 #scenario data file path
-supp_path = r"C:\Users\chenl\OneDrive - UBC\Desktop\bw2\hydrogen superstructure\files\CaseStudy\scenario_CaseStudy.xlsx"
+supp_path = '/Users/Bonnie/Desktop/demo/scenario_demo.xlsx'
+#all exported file folder
+output_path = '/Users/Bonnie/Desktop/demo/results'
+
+'''Monte Carlo Simulatio part'''
+#Monte Carlo Simulation project name
+mc_project = 'mc'
+#Monte Carlo Simulation iterations
+iterations = 1000
+#Monte Carlo Simulation file path
+mc_path = '/Users/Bonnie/Desktop/demo/mc_demo.xlsx'
+
+'''edit after running the code'''
+#brightway2 compatible inventory table file path
+input_file_path = '/Users/Bonnie/Desktop/demo/results/input_demo.xlsx'
+#calculated LCIA result file path
+LCA_result_path = '/Users/Bonnie/Desktop/demo/results/LCA results_demo.xlsx'
 
 
 #avoid overwritting
